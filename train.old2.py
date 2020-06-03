ff2f5d272909bce4d0109c0849e3ac9c8c07763f
alacrois <alacrois@student.42.fr>
2020-06-03 20:47:28 +0200

    ..

--- train.old2.py
+++ train.old2.py
@@ -11,15 +11,17 @@ t1 = float(lines[1])
 def pe(km):
     return t0 + t1 * km
 
+data = []
 dataX = []
 dataY = []
 
-f = open('data2.csv', 'r')
+f = open('data.csv', 'r')
 lines = f.read().split('\n')
 f.close()
 for i in range(1, len(lines)):
     line = lines[i]
     if len(line.split(",")) > 1 :
+        data.append([line.split(",")[0], line.split(",")[1].strip("\n")])
         dataX.append(float(line.split(",")[0]))
         dataY.append(float(line.split(",")[1]))
 
@@ -32,7 +34,7 @@ dataX_range = maxX - minX
 dataY_range = maxY - minY
 dataX_normalized = []
 dataY_normalized = []
-for i in range(len(dataX)):
+for i in range(len(data)):
     dataX_normalized.append(dataX[i] / dataX_range)
     dataY_normalized.append(dataY[i] / dataY_range)
     # dataX_normalized.append((dataX[i] - minX) / (maxX - minX))
@@ -40,50 +42,61 @@ for i in range(len(dataX)):
 
 # Some functions...
 def cost(a, b):
-    s = 0
-    for i in range(len(dataX)):
+    error = 0
+    for i in range(len(data)):
+        #old :
+        # km = float(data[i][0])
+        # p = float(data[i][1])
+        #new :
         # km = dataX[i]
         # p = dataY[i]
         km = dataX_normalized[i]
         p = dataY_normalized[i]
-        s += ((a * km + b) - p)**2
-    s = s / (len(dataX) * 2)
-    return s
+
+        # print(str(i) + " : km(x) = " + str(km) + " & p(y) = " + str(p))
+        error += ((a * km + b) - p)**2
+        # error += p - (a + b * km)
+    error = error / (len(data) * 2)
+    # print("len(data) = " + str(len(data)))
+    return error
 
 def der_cost_a(a, b):
     s = 0
-    for i in range(len(dataX)):
+    for i in range(len(data)):
+        #old :
+        # km = float(data[i][0])
+        # p = float(data[i][1])
+        #new :
         # km = dataX[i]
         # p = dataY[i]
         km = dataX_normalized[i]
         p = dataY_normalized[i]
 
         s += km * ((a * km + b) - p)
-    s = s / len(dataX)
+    s = s / len(data)
     return s
 
 def der_cost_b(a, b):
     s = 0
-    for i in range(len(dataX)):
+    for i in range(len(data)):
+        #old :
+        # km = float(data[i][0])
+        # p = float(data[i][1])
+        #new :
         # km = dataX[i]
         # p = dataY[i]
         km = dataX_normalized[i]
         p = dataY_normalized[i]
 
         s += ((a * km + b) - p)
-    s = s / len(dataX)
+    s = s / len(data)
     return s
 
-def get_predictions(tmp_t0, tmp_t1, denormalize) :
-    if denormalize == True :
-        tmp_t0 = tmp_t0 * (dataY_range)
-        tmp_t1 = tmp_t1 * (dataY_range / dataX_range)
+def get_predictions(tmp_t0, tmp_t1) :
     predictions = []
     for i in range(len(dataX)) :
-        if denormalize == True :
-            predictions.append(tmp_t0 + tmp_t1 * dataX[i])
-        else :
-            predictions.append(tmp_t0 + tmp_t1 * dataX_normalized[i])
+        # predictions.append(tmp_t0 + tmp_t1 * dataX[i])
+        predictions.append(tmp_t0 + tmp_t1 * dataX_normalized[i])
     return predictions
 
 all_predictions = []
@@ -95,8 +108,8 @@ iterations = 500
 # t0 = 10000
 # t1 = -0.03
 
-# t0 = 0
-# t1 = 0
+t0 = 0
+t1 = 0
 
 print("Starting...  t0 = " + str(t0) + "  t1 = " + str(t1) + "\n")
 print("cost(t1, t0) = " + str(cost(t1, t0)) + "\n")
@@ -107,30 +120,25 @@ learning_rate = 0.5
 for every in range(iterations):
     tmp_t0 = t0
     tmp_t1 = t1
-    # print("der_cost_a(tmp_t1, tmp_t0) = " + str(der_cost_a(tmp_t1, tmp_t0)))
-    # print("der_cost_b(tmp_t1, tmp_t0) = " + str(der_cost_b(tmp_t1, tmp_t0)))
+    print("der_cost_a(tmp_t1, tmp_t0) = " + str(der_cost_a(tmp_t1, tmp_t0)))
+    print("der_cost_b(tmp_t1, tmp_t0) = " + str(der_cost_b(tmp_t1, tmp_t0)))
     t1 = t1 - learning_rate * der_cost_a(tmp_t1, tmp_t0)
     t0 = t0 - learning_rate * der_cost_b(tmp_t1, tmp_t0)
-    # print("One iterations done ==> t0 = " + str(t0) + " & t1 = " + str(t1))
-    # print("cost(t1, t0) = " + str(cost(t1, t0)) + "\n")
+    # t0 = tmp_t0
+    # t1 = tmp_t1
+    print("One iterations done ==> t0 = " + str(t0) + " & t1 = " + str(t1))
+    print("cost(t1, t0) = " + str(cost(t1, t0)) + "\n")
+    # learning_rate = learning_rate / 2
     if every == 0 or every == int(iterations / 4) or every == iterations - 1:
-        all_predictions.append(get_predictions(t0, t1, False))
+        all_predictions.append(get_predictions(t0, t1))
 
 
 # plt.scatter(dataX, dataY, c='k')
-# plt.plot(dataX, predictions, c='r')
-
 plt.scatter(dataX_normalized, dataY_normalized, c='k')
+# plt.plot(dataX, predictions, c='r')
 plt.scatter(dataX_normalized, all_predictions[0], c='r')
 plt.scatter(dataX_normalized, all_predictions[1], c='b')
 plt.scatter(dataX_normalized, all_predictions[2], c='g')
-
-# plt.scatter(dataX, dataY, c='k')
-# plt.scatter(dataX, all_predictions[0], c='r')
-# plt.scatter(dataX, all_predictions[1], c='b')
-# plt.scatter(dataX, all_predictions[2], c='g')
-
-
 plt.show()
 print("Finished " + str(iterations) + " iterations ! ==> t0 = " + str(t0) + "  t1 = " + str(t1))
 
@@ -138,14 +146,13 @@ print("Finished " + str(iterations) + " iterations ! ==> t0 = " + str(t0) + "  t
 t0_final = t0 * (dataY_range)
 t1_final = t1 * (dataY_range / dataX_range)
 
-print("t0_final = " + str(t0_final))
-print("t1_final = " + str(t1_final))
+print("t0_corrected = " + str(t0_corrected))
+print("t1_corrected = " + str(t1_corrected))
 
 for i in range(len(dataX)):
-    p = t0_final + t1_final * dataX[i]
+    p = t0_corrected + t1_corrected * dataX[i]
     print("dataX[" + str(i) + "] = " + str(dataX[i]) + " & prediction = " + str(p))
-
-# Writing new thetas to file...
-f = open('thetas', 'w')
-f.write(str(t0_final) + "\n" + str(t1_final) + "\n")
-f.close()
+# for i in range(len(data)):
+#     print("dataX[" + str(i) + "] = " + str(dataX[i]))
+#     print("dataY[" + str(i) + "] = " + str(dataY[i]))
+# print("min(dataX) = " + str(min(dataX)))
