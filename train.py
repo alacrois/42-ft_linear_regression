90aa2961944014fc60873758ea30ba42c7c49b44
alacrois <alacrois@student.42.fr>
2020-06-03 19:30:02 +0200

    ..

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math
 
 f = open('thetas', 'r')
 lines = f.read().split('\n')
@@ -32,18 +33,22 @@ maxX = max(dataX)
 dataX_normalized = []
 dataY_normalized = []
 for i in range(len(data)):
-    dataX_normalized.append((dataX[i] - minX) / (maxX - minX))
-    dataY_normalized.append((dataY[i] - minY) / (maxY - minY))
+    dataX_normalized.append(dataX[i] / (maxX - minX))
+    dataY_normalized.append(dataY[i] / (maxY - minY))
+    # dataX_normalized.append((dataX[i] - minX) / (maxX - minX))
+    # dataY_normalized.append((dataY[i] - minY) / (maxY - minY))
 
 def cost(a, b):
     error = 0
     for i in range(len(data)):
         #old :
-        km = float(data[i][0])
-        p = float(data[i][1])
+        # km = float(data[i][0])
+        # p = float(data[i][1])
         #new :
         # km = dataX[i]
         # p = dataY[i]
+        km = dataX_normalized[i]
+        p = dataY_normalized[i]
 
         # print(str(i) + " : km(x) = " + str(km) + " & p(y) = " + str(p))
         error += ((a * km + b) - p)**2
@@ -56,11 +61,13 @@ def der_cost_a(a, b):
     s = 0
     for i in range(len(data)):
         #old :
-        km = float(data[i][0])
-        p = float(data[i][1])
+        # km = float(data[i][0])
+        # p = float(data[i][1])
         #new :
         # km = dataX[i]
         # p = dataY[i]
+        km = dataX_normalized[i]
+        p = dataY_normalized[i]
 
         s += km * ((a * km + b) - p)
     s = s / len(data)
@@ -70,15 +77,26 @@ def der_cost_b(a, b):
     s = 0
     for i in range(len(data)):
         #old :
-        km = float(data[i][0])
-        p = float(data[i][1])
+        # km = float(data[i][0])
+        # p = float(data[i][1])
         #new :
         # km = dataX[i]
         # p = dataY[i]
+        km = dataX_normalized[i]
+        p = dataY_normalized[i]
 
         s += ((a * km + b) - p)
     s = s / len(data)
     return s
+
+def get_predictions(tmp_t0, tmp_t1) :
+    predictions = []
+    for i in range(len(dataX)) :
+        # predictions.append(tmp_t0 + tmp_t1 * dataX[i])
+        predictions.append(tmp_t0 + tmp_t1 * dataX_normalized[i])
+    return predictions
+
+all_predictions = []
 
 iterations = 500
 # tmp_t0 = t0
@@ -87,12 +105,15 @@ iterations = 500
 # t0 = 10000
 # t1 = -0.03
 
+t0 = 0
+t1 = 0
+
 print("Starting...  t0 = " + str(t0) + "  t1 = " + str(t1) + "\n")
 print("cost(t1, t0) = " + str(cost(t1, t0)) + "\n")
 
-
 # learning_rate = 0.000000000015
-learning_rate = 0.0000000001
+# learning_rate = 0.0000000001
+learning_rate = 0.25
 for every in range(iterations):
     tmp_t0 = t0
     tmp_t1 = t1
@@ -105,13 +126,16 @@ for every in range(iterations):
     print("One iterations done ==> t0 = " + str(t0) + " & t1 = " + str(t1))
     print("cost(t1, t0) = " + str(cost(t1, t0)) + "\n")
     # learning_rate = learning_rate / 2
+    if every == 0 or every == int(iterations / 4) or every == iterations - 1:
+        all_predictions.append(get_predictions(t0, t1))
 
-predictions = []
-for i in range(len(dataX)) :
-    predictions.append(t0 + t1 * dataX[i])
-plt.scatter(dataX, dataY)
-plt.scatter(dataX_normalized, dataY_normalized)
-plt.plot(dataX, predictions, c='r')
+
+# plt.scatter(dataX, dataY, c='k')
+plt.scatter(dataX_normalized, dataY_normalized, c='k')
+# plt.plot(dataX, predictions, c='r')
+plt.scatter(dataX_normalized, all_predictions[0], c='r')
+plt.scatter(dataX_normalized, all_predictions[1], c='b')
+plt.scatter(dataX_normalized, all_predictions[2], c='g')
 plt.show()
 print("Finished " + str(iterations) + " iterations ! ==> t0 = " + str(t0) + "  t1 = " + str(t1))
 
