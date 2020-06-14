d9d6cd8845b1426e571f267568b73b09f0143e60
alacrois <alacrois@student.42.fr>
2020-06-14 17:27:15 +0200

    ..

--- trainv2.py
+++ trainv2.py
@@ -10,6 +10,18 @@ print(thetas)
 
 # Getting dataset
 dataX, dataY = parsing.read_data()
+x, y = np.array(dataX), np.array(dataY)
+x, y = x.reshape(x.shape[0], 1), y.reshape(y.shape[0], 1)
+print("shapes = " + str(x.shape) + "    " + str(y.shape))
+
+X = np.hstack((x, np.ones(x.shape)))
+print(X)
+
+theta = np.array(thetas)
+theta = theta.reshape(theta.shape[0], 1)
+print(theta)
+print("shape = " + str(theta.shape))
+
 m = len(dataX)
 
 # Saving new thetas values to file
